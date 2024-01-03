cad2b172c35f54c5fd03fe6e765d43542cdadcea
Abhijit Das <abhijitd721201@gmail.com>
2024-01-03 16:02:44 +0530

    Fragnents

---
+++ 18_Fragments/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <center>
+      <div>
+        <h1>Healthy Food List..</h1>
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+    </center>
+  );
+}
+
+export default App;
