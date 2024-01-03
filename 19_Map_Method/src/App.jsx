0119273260b27536a69a7bd7416cf70e2f1a1a31
Abhijit Das <abhijitd721201@gmail.com>
2024-01-03 16:58:34 +0530

    mapping the items

---
+++ 19_Map_Method/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  let foodItems = ["Dall", "Green Vegitable", "Roti", "Milk", "Salad"];
+  return (
+    <>
+      <center>
+        <h1>Healthy Food List..</h1>
+
+        <ul class="list-group">
+          {foodItems.map((list) => (
+            <li class="list-group-item">{list}</li>
+          ))}
+        </ul>
+      </center>
+    </>
+  );
+}
+
+export default App;
