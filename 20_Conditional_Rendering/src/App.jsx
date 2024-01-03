1288c55b095ac7c2ff2c289538368b6f5ac55342
Abhijit Das <abhijitd721201@gmail.com>
2024-01-03 17:55:35 +0530

    Use Tarnari operator

--- 20_Conditional_Rendering/src/App.jsx
+++ 20_Conditional_Rendering/src/App.jsx
@@ -15,14 +15,13 @@ function App() {
     "Banana",
   ];
 */
-  if (foodItems.length === 0) {
-    return <h3>I am hungry</h3>;
-  }
 
   return (
     <>
       <center>
         <h1>Healthy Food List..</h1>
+        {foodItems.length === 0 ? <h3>I am hungry</h3> : null}
+
         <ul class="list-group">
           {foodItems.map((list) => (
             <li class="list-group-item">{list}</li>
