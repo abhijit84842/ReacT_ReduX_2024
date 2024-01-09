89ebe8c479973ab250916ce6c39b29884852575d
Abhijit Das <abhijitd721201@gmail.com>
2024-01-09 15:35:53 +0530

    update

--- 25_Passing_Function_Via_Props/src/App.jsx
+++ 25_Passing_Function_Via_Props/src/App.jsx
@@ -2,21 +2,17 @@ import FoodList from "./Components/FoodList";
 import ErrorMsg from "./Components/ErrorMsg";
 import "./App.css";
 import Container from "./Components/Container";
+import CustomBox from "./Components/CustomBox";
+
 function App() {
   let foodItems = ["Chicken", "Motton", "Egg", "Banana"];
   return (
     <>
       <Container>
         <h1 className="food-heading">Protein Food Items List</h1>
+        <CustomBox></CustomBox>
         <FoodList items={foodItems}></FoodList>
         <ErrorMsg items={foodItems}></ErrorMsg>
-      </Container>
-
-      <Container>
-        <p>
-          Above is the list of healthy food thats are good for health and well
-          being.
-        </p>
       </Container>
     </>
   );
--- 25_Passing_Function_Via_Props/src/Components/IndvItem.jsx
+++ 25_Passing_Function_Via_Props/src/Components/IndvItem.jsx
@@ -2,9 +2,23 @@ import style from "./IndvItem.module.css";
 const Item = ({ foodlist }) => {
   // destructring the data from array
 
+  const handleBuyButtonClicked = (event) => {
+    // get the event object here
+
+    console.log(event); // print the event object
+
+    console.log(`${foodlist} being bought..`);
+  };
+
   return (
     <li className={`${style["kg-item"]} list-group-item `}>
       <span className={style["kg-span"]}>{foodlist}</span>
+      <button
+        className={`${style.button} btn btn-success`}
+        onClick={(event) => handleBuyButtonClicked(event)} // pass the event object
+      >
+        Buy
+      </button>
     </li>
   );
 };
